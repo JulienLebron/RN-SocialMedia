b7bf17cfefaf216e7d1f1db12eb445b245c766aa
Julien Lebron <lebron-pro@outlook.fr>
2025-07-31 21:47:06 +0200

    React Native Gotchas

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,11 +5,26 @@ import {
   DefaultTheme,
   DarkTheme,
 } from "@react-navigation/native";
+import { AppState, Platform } from "react-native";
 import { AuthProvider } from "@/providers/AuthProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  onlineManager,
+  focusManager,
+} from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
+import NetInfo from "@react-native-community/netinfo";
+import type { AppStateStatus } from "react-native";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
+
+onlineManager.setEventListener((setOnline) => {
+  return NetInfo.addEventListener((state) => {
+    setOnline(!!state.isConnected);
+  });
+});
 
 const CustomTheme = {
   ...DefaultTheme,
@@ -22,6 +37,18 @@ const CustomTheme = {
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
+
+  function onAppStateChange(status: AppStateStatus) {
+    if (Platform.OS !== "web") {
+      focusManager.setFocused(status === "active");
+    }
+  }
+
+  useEffect(() => {
+    const subscription = AppState.addEventListener("change", onAppStateChange);
+
+    return () => subscription.remove();
+  }, []);
 
   return (
     <QueryClientProvider client={queryClient}>
